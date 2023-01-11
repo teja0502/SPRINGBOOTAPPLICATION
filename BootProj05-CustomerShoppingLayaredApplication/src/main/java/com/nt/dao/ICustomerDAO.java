17f14c1301e3fb65382769cec78696deba75fb6d
teja <teja@gmail.com>
2023-01-11 16:16:29 +0530

    Commit

---
+++ BootProj05-CustomerShoppingLayaredApplication/src/main/java/com/nt/dao/ICustomerDAO.java
@@ -0,0 +1,9 @@
+package com.nt.dao;
+
+import com.nt.model.Customer;
+
+public interface ICustomerDAO {
+	
+	public int insertCustomer(Customer cust) throws Exception;
+
+}
---
+++ BootProj05-CustomerShoppingLayaredApplication/src/main/java/com/nt/service/ICustomerMgmtService.java
@@ -0,0 +1,9 @@
+package com.nt.service;
+
+import com.nt.model.Customer;
+
+public interface ICustomerMgmtService {
+	
+	public String registerCustomer(Customer cust) throws Exception;
+
+}
